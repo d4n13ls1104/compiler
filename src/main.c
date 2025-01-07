38a222f7e0e2917310b8b7e0005cd8713ed54fdf
lobsterpenis <andrewdaniels060@gmail.com>
2025-01-06 19:27:55 -0500

    fix indentation

--- src/main.c
+++ src/main.c
@@ -8,27 +8,27 @@
 
 u0 printf_token(u64 type, u64 data, u8 * str)
 {
-	printf("type: %s, data: 0x%016lX, str: '%s'\n",
-			token_type_to_str(type), data, str);
+  printf("type: %s, data: 0x%016lX, str: '%s'\n", 
+      token_type_to_str(type), data, str);
 }
 
 u0 cc(u8 * path)
 {
-	u8  le[MAXSTR];
-	u64 type, data;
+  u8  le[MAXSTR];
+  u64 type, data;
 
-	lexer_load_source(path);
+  lexer_load_source(path);
 
-	while ((type = lex(&data, le)) != TK_EOF)
-		printf_token(type, data, le);
+  while ((type = lex(&data, le)) != TK_EOF)
+    printf_token(type, data, le);
 		
-	/*printf_symtab();*/
+  /*printf_symtab();*/
 }
 
 int main(int argc, char * argv[])
 {
-	if (argc != 2)
-		die("invalid argc");
-	cc(argv[1]);
-	return 0;
+  if (argc != 2)
+    die("invalid argc");
+  cc(argv[1]);
+  return 0;
 }
--- src/util.c
+++ src/util.c
@@ -35,22 +35,22 @@ u8 * readfile(u8 * path)
 
 u8 * token_type_to_str(u64 i)
 {
-	switch (i) {
-	case TK_EOF:
-		return "EOF";
-	case TK_IDENTIFIER:
-		return "IDENTIFIER";
-	case TK_KEYWORD:
-		return "KEYWORD";
-	case TK_INTEGER:
-		return "INTEGER";
-	case TK_OPERATOR:
-		return "OPERATOR";
-	case TK_SEPARATOR:
-		return "SEPARATOR";
-	case TK_UNKNOWN:
-		return "UNKNOWN";
-	}
+  switch (i) {
+  case TK_EOF:
+    return "EOF";
+  case TK_IDENTIFIER:
+    return "IDENTIFIER";
+  case TK_KEYWORD:
+    return "KEYWORD";
+  case TK_INTEGER:
+    return "INTEGER";
+  case TK_OPERATOR:
+    return "OPERATOR";
+  case TK_SEPARATOR:
+    return "SEPARATOR";
+  case TK_UNKNOWN:
+    return "UNKNOWN";
+  }
 }
 
 bool kw_is_datatype(u64 i)
